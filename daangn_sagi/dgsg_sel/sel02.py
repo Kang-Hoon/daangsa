3977e54f4c041dc585552783441f68850a880c11
Kang-Hoon Lee <khlee3179@gmail.com>
2022-05-03 22:29:39 +0900

    May 3rd
    May 3rd (2)

--- daangn_sagi/dgsg_sel/sel02.py
+++ daangn_sagi/dgsg_sel/sel02.py
@@ -9,7 +9,7 @@ url1 = "https://cafe.naver.com/joonggonara"
 driver.get(url1)
 
 driver.find_element_by_css_selector('#topLayerQueryInput').send_keys('오픈소스')
-driver.find_element_by_css_selector('').send_keys(Keys.ENTER)
+driver.find_element_by_css_selector('#topLayerQueryInput').send_keys(Keys.ENTER)
 
 
 # <input title="카페글 검색어 입력" type="text" class="inp" name="query" style="ime-mode:active" id="topLayerQueryInput" onkeydown="if (event.keyCode == 13) {searchBoard();}">
